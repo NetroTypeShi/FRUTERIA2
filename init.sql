072685b402127c3efe040fc5756a054af0ebc222
netro <netro@carti.myguest.virtualbox.org>
2025-04-02 00:05:53 +0200

    siu

---
+++ init.sql
@@ -0,0 +1,5 @@
+-- Crea la tabla "fruits" si no existe
+CREATE TABLE IF NOT EXISTS fruits (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = require("express")
+const express = require('express');
+const Database = require('better-sqlite3');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static('public')); 
 
-const frutas = ["manzana"]
 
-app.get("/frutas", (req, res)=>{
-    const fruta = req.body.fruta
-    frutas.push(fruta)
-    res.json(frutas);
-})
-app.listen(3000, () => console.log("Servidor en 3000"));
+const db = new Database('database.db');
+
+
+app.get("/frutas", (req, res) => {
+  const query = db.prepare("SELECT name FROM fruits");
+  const fruits = query.all().map(row => row.name);
+  res.json(fruits);
+});
+
+app.post("/frutas", (req, res) => {
+  const { fruta } = req.body;
+  const insert = db.prepare("INSERT INTO fruits (name) VALUES (?)");
+  insert.run(fruta);
+  //parte json
+  const query = db.prepare("SELECT * FROM fruits");
+  const fruits = query.all();
+  res.json(fruits);
+});
+////});
+
+app.listen(3000, () => {
+  console.log("Servidor iniciado en el puerto 3000");
+});
